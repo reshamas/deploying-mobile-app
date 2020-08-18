abd7fe39569b0b139502512a479dcc60aae2eaa0
nup0013 <nidhin.pattaniyil@walmart.com>
2020-08-18 19:50:39 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # deploying-mobile-app
+
+
+```
+npm install -g expo-cli
+expo init mobile-app
+
+cd mobile-app
+npm start # you can also use: expo start
+
+```
+
+
+```
+- cd mobile-app
+- yarn start # you can open iOS, Android, or web from here, or run them directly with the commands below.
+- yarn android
+- yarn ios
+- yarn web
+```
---
+++ config.tsx
@@ -0,0 +1,24 @@
+const AppConfig = 
+
+{
+    title : "What Image Is It?"
+    ,description: `This is normal text
+    
+This mobile app was developed by 
+- [Nidhin Pattaniyil](https://npatta01.github.io)
+- [Reshama Shaikh](https://reshamas.github.io/)
+
+This app lets you submit a photo of food and returns the predicted food category. 🍕
+
+The model was developed using the [food-101 dataset](https://www.vision.ee.ethz.ch/datasets_extra/food-101/) and the [fastai](https://github.com/fastai/fastai) deep learning library, which is built on PyTorch.
+
+
+# h1 Heading 8-)
+
+**This is some bold text!**
+    
+    `
+};
+
+
+export {AppConfig}
